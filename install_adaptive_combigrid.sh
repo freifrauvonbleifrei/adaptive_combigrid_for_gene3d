8f9a5aa45bdc488b83dd010014c3d88146b8b668
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2021-02-23 07:39:33 +0100

    renamed patch

--- install_adaptive_combigrid.sh
+++ install_adaptive_combigrid.sh
@@ -7,11 +7,12 @@ cd spack-sgpp
 git am < ../adaptive_combigrid_convenience.patch
 ./bin/spack compiler find
 ./bin/spack spec sgpp@adaptive_combigrid_convenience+combigrid+python
-# in case of version conflicts, may need to restrict dependency versions further:
-#./bin/spack spec sgpp@adaptive_combigrid_convenience+combigrid+python^py-numpy@1.17:1.18^py-scipy@1.3:1.5
+# in case of version conflicts, may need to restrict dependency versions further
+# e.g., on my laptop:
+#./bin/spack spec sgpp@adaptive_combigrid_convenience+combigrid+python^py-numpy@1.17:1.18^py-scipy@1.3:1.5^py-setuptools@:40
 
 # at this point, you can tell spack which components are already installed by
-# editing .etc/spack/packages.yaml -- this avoids re-installing everything
+# editing ./etc/spack/packages.yaml -- this avoids re-installing everything
 # cf. https://spack.readthedocs.io/en/latest/build_settings.html
 
 # this here can take a long time:
