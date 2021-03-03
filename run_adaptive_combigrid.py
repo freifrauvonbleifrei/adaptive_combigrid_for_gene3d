d5afbbeed905552c5123cd2c91d14441313204ca
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2021-03-03 16:45:33 +0100

    run_adaptive: use minimum level from parameters

--- run_adaptive_combigrid.py
+++ run_adaptive_combigrid.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
+from os import environ
 import dimadapt2d
 
 # restricted w to 64, because `Gauss-Laguerre integration is currently not possible for nw0>124.`
 
 print("Running with adaptation only to target resolution")
-adapt = dimadapt2d.DimensionalAdaptation([5,5,5,5,3], [10,10,10,10,6], output3d=True)
+adapt = dimadapt2d.DimensionalAdaptation(environ.get('ADAPTATION_MINIMUM_LEVEL'), [10,10,10,10,6], output3d=True)
 
 adapt.run_dimadapt_algorithm(100)
-
