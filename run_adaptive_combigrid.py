9cfcc2646a1369255581f371b637ceaee10a71e9
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2021-03-04 09:44:38 +0100

    correct reading of lmin (fixes d5afbbeed9)

--- run_adaptive_combigrid.py
+++ run_adaptive_combigrid.py
@@ -6,6 +6,8 @@ import dimadapt2d
 # restricted w to 64, because `Gauss-Laguerre integration is currently not possible for nw0>124.`
 
 print("Running with adaptation only to target resolution")
-adapt = dimadapt2d.DimensionalAdaptation(environ.get('ADAPTATION_MINIMUM_LEVEL'), [10,10,10,10,6], output3d=True)
+# cf. https://stackoverflow.com/questions/48066106/converting-vector-looking-string-into-vector-values-python
+lmin=[int(i.strip()) for i in environ.get('ADAPTATION_MINIMUM_LEVEL')[1:-1].split(",")]
+adapt = dimadapt2d.DimensionalAdaptation(lmin, [10,10,10,10,6], output3d=True)
 
 adapt.run_dimadapt_algorithm(100)
