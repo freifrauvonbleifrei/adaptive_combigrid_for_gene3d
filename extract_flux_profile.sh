c48daa2e7389e89d02aa8cc35a303994c4911f03
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2021-03-04 08:42:20 +0100

    optimized extract_flux_profile.sh (cherry-picked simsgs.informatik.uni-stuttgart.de:EXAHD/ParameterFiles.git @ 2605aba60d8178

--- extract_flux_profile.sh
+++ extract_flux_profile.sh
@@ -1,50 +1,52 @@
 #!/usr/bin/env bash
-#module unload python-site/3.6
-module load python/3.6_intel
 
 . ./adaptation_parameters.sh
 
 PROB_PREPATH=${ADAPTATION_PROB_PATH}
-DIAG_PATH=${ADAPTATION_GENE3D_PATH}/diag-python/
+DIAG_PATH="./gene-diag"
 DIAGNOSTICS=DiagFluxesgene3d
 OUTDIR='./flux_diags/'
 mkdir -p $OUTDIR
-for PROBNAME in $PROB_PREPATH/prob_5*; do
-	PROBNAME=$(basename $PROBNAME)
-	#PROBNAME='prob_5_5_5_5_3'
-	#PROBNAME='kjm_prob_5_7_8_5_3'
-	if [ -d $PROB_PREPATH/${PROBNAME} ]; then
-		echo $PROBNAME
-		#totallasttime=0.0
-		#for f in `ls $PROBNAME/nrg*`; do
-		#    lasttime=$(tail -n 2 $f | head -n 1)
-		#    echo "lasttime of file $f is $lasttime"
-		#    if (( $(echo "$lasttime > $totallasttime" |bc -l) )); then
-		#	endtime=$(tail -n 4 $f |head -n 1)
-		#    fi
-		#done
-		#python3 ./diag-python/GENE_gui_python/gene_cl.py -i ./$PROBNAME/out -o $OUTDIR --starttime=800.0 --endtime=$endtime -e '_1' '_2' '_3' '_4' '_5' '_6' '_7' '_8' '_9' '_10' '_11' '_12' '_13' '_14' '_15' '_16' '_17' '_18' '_19' '_20' '_21' '_22' '_23' '.dat'
-		NRGFILESLIST=""
-		# https://unix.stackexchange.com/questions/239772/bash-iterate-file-list-except-when-empty
-		shopt -s nullglob
-		for NRGFILENUM in $PROB_PREPATH/$PROBNAME/out/nrg_*; do
-			echo $NRGFILENUM
-			NRGFILENUM=$(basename $NRGFILENUM)
-			NRGSUBSTRING=$(echo $NRGFILENUM | cut -d'_' -f 2)
-			NRGFILESLIST+=" _${NRGSUBSTRING}"
-			#echo $NRGFILESLIST
-		done
-		NRGFILESLIST+=" .dat"
-		#python3 ./diag-python/GENE_gui_python/gene_cl.py -i $PROB_PREPATH/$PROBNAME/out -o $OUTDIR --starttime=150.0 -d${DIAGNOSTICS} -e '_1' '_2' '_3' '_4' '_5' '_6' '_7' '_8' '_9' '_10' '_11' '_12' '_13' '_14' '_15' '_16' '_17' '_18' '_19' '_20' '_21' '_22' '_23' #'.dat'
-		python3 ${DIAG_PATH}/GENE_gui_python/gene_cl.py -i $PROB_PREPATH/$PROBNAME/out -o $OUTDIR --starttime=150.0 -d${DIAGNOSTICS} -e ${NRGFILESLIST}
+for PROBNAME in $PROB_PREPATH/prob_*; do
+    PROBNAME=$(basename $PROBNAME)
+    #PROBNAME='prob_5_5_5_5_3'
+    #PROBNAME='kjm_prob_5_7_8_5_3'
+    if [ -d $PROB_PREPATH/${PROBNAME} ]; then
+        echo $PROBNAME
+        #totallasttime=0.0
+        #for f in `ls $PROBNAME/nrg*`; do
+        #    lasttime=$(tail -n 2 $f | head -n 1)
+        #    echo "lasttime of file $f is $lasttime"
+        #    if (( $(echo "$lasttime > $totallasttime" |bc -l) )); then
+        #       endtime=$(tail -n 4 $f |head -n 1)
+        #    fi
+        #done
+        # only re-run the diagnostic if the nrg.dat file is newer than the existing flux profile output
+        if [ ! -e $OUTDIR/flux_profile_ions_$PROBNAME.h5 ] || [ $PROB_PREPATH/$PROBNAME/out/nrg.dat -nt $OUTDIR/flux_profile_ions_$PROBNAME.h5 ] ; then
+            #python3 ./diag-python/GENE_gui_python/gene_cl.py -i ./$PROBNAME/out -o $OUTDIR --starttime=800.0 --endtime=$endtime -e '_1' '_2' '_3' '_4' '_5' '_6' '_7' '_8' '_9' '_10' '_11' '_12' '_13' '_14' '_15' '_16' '_17' '_18' '_19' '_20' '_21' '_22' '_23' '.dat'
+            NRGFILESLIST=""
+            # https://unix.stackexchange.com/questions/239772/bash-iterate-file-list-except-when-empty
+            shopt -s nullglob
+            for NRGFILENUM in $PROB_PREPATH/$PROBNAME/out/nrg_*; do
+                echo $NRGFILENUM
+                NRGFILENUM=$(basename $NRGFILENUM)
+                NRGSUBSTRING=$(echo $NRGFILENUM | cut -d'_' -f 2)
+                NRGFILESLIST+=" _${NRGSUBSTRING}"
+            done
+            NRGFILESLIST+=" .dat"
+            #python3 ./diag-python/GENE_gui_python/gene_cl.py -i $PROB_PREPATH/$PROBNAME/out -o $OUTDIR --starttime=150.0 -d${DIAGNOSTICS} -e '_1' '_2' '_3' '_4' '_5' '_6' '_7' '_8' '_9' '_10' '_11' '_12' '_13' '_14' '_15' '_16' '_17' '_18' '_19' '_20' '_21' '_22' '_23' #'.dat'
+            python3 ${DIAG_PATH}/GENE_gui_python/gene_cl.py -i $PROB_PREPATH/$PROBNAME/out -o $OUTDIR --starttime=150.0 -d${DIAGNOSTICS} -e ${NRGFILESLIST}
 
-		mv $OUTDIR/flux_profile_ions_.h5 $OUTDIR/flux_profile_ions_$PROBNAME.h5
-		mv $OUTDIR/flux_profile_electrons_.h5 $OUTDIR/flux_profile_electrons_$PROBNAME.h5
-		RETURNCODE=$?
-		if [ $RETURNCODE -ne 0 ]; then
-			echo $PROBNAME >>$OUTDIR/probnames_that_did_not_work
-		fi
-	else
-		echo $PROBNAME not there!
-	fi
+            mv $OUTDIR/flux_profile_ions_.h5 $OUTDIR/flux_profile_ions_$PROBNAME.h5
+            if [ ${ADAPTATION_NUMBER_OF_SPECIES} = "2" ] ; then
+                mv $OUTDIR/flux_profile_electrons_.h5 $OUTDIR/flux_profile_electrons_$PROBNAME.h5
+            fi
+        fi
+        RETURNCODE=$?
+        if [ $RETURNCODE -ne 0 ]; then
+            echo $PROBNAME >>$OUTDIR/flux_probnames_that_did_not_work
+        fi
+    else
+        echo $PROBNAME not there!
+    fi
 done
