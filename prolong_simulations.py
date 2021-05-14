91abe2b6e8a49a15cadb80588138a819584b616c
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2021-05-14 16:53:31 +0200

    prolong scripts: allow calling from python and sh

--- prolong_simulations.py
+++ prolong_simulations.py
@@ -3,25 +3,32 @@
 from Qes_data import Qes_data
 import sim_launcher
 from write_flux_adaptive_combigrid import get_combiScheme
+import sys
 
-combiSchemeMode = sys.argv[1]
-newStartTime = sys.argv[2]
-newEndTime = sys.argv[3]
 
-combiScheme = get_combiScheme("", combiSchemeMode, dropzeros=True)
+def prolong(combiSchemeMode, newStartTime, newEndTime):
+    combiScheme = get_combiScheme("", combiSchemeMode, dropzeros=True)
 
-d = 0
-while str('l_'+str(d+1)) in combiScheme:
-    d += 1
+    d = 0
+    while str('l_'+str(d+1)) in combiScheme:
+        d += 1
 
-for i, row in combiScheme.iterrows():
     level = [0]*d
     lkeys = [str('l_'+str(dim)) for dim in range(d)]
-    for dim in range(d):
-        level[dim] = combiScheme[lkeys[dim]]
-    # check if current simulation is finished and has run long enough
-    assert(sim_launcher.check_finished(level_vector=level))
-    assert(Qes_data(level_vector=level).how_long_run() > 0.99 * newStartTime)
-    # but not too long
-    assert(Qes_data(level_vector=level).how_long_run() < 1.01 * newStartTime)
-    sim_launcher.restart_sim(level_vector=level, newSimTime=newEndTime)
+
+    for i, row in combiScheme.iterrows():
+        for dim in range(d):
+            level[dim] = row[lkeys[dim]]
+        # check if current simulation is finished and has run long enough
+        assert(sim_launcher.check_finished(level_vector=level))
+        assert(Qes_data(level_vector=level).how_long_run() > 0.99 * newStartTime)
+        # but not too long
+        assert(Qes_data(level_vector=level).how_long_run() < 1.01 * newStartTime)
+        sim_launcher.restart_sim(level_vector=level, newSimTime=newEndTime)
+
+
+if __name__ == "__main__":
+    combiSchemeMode = sys.argv[1]
+    newStartTime = sys.argv[2]
+    newEndTime = sys.argv[3]
+    prolong(combiSchemeMode, newStartTime, newEndTime)
--- wait_for_simulations.py
+++ wait_for_simulations.py
@@ -6,30 +6,34 @@ import time
 from Qes_data import Qes_data
 from write_flux_adaptive_combigrid import get_combiScheme
 
-combiSchemeMode = sys.argv[1]
-newEndTime = sys.argv[2]
 
-combiScheme = get_combiScheme("", combiSchemeMode, dropzeros=True)
+def wait(combiSchemeMode, newEndTime):
+    combiScheme = get_combiScheme("", combiSchemeMode, dropzeros=True)
 
-d = 0
-while str('l_'+str(d+1)) in combiScheme.columns:
-    d += 1
+    d = 0
+    while str('l_'+str(d+1)) in combiScheme.columns:
+        d += 1
+    level = [0]*d
+    lkeys = [str('l_'+str(dim)) for dim in range(d)]
 
-while len(combiScheme) > 0:
-    for i, row in combiScheme.iterrows():
-        level = [0]*d
-        lkeys = [str('l_'+str(dim)) for dim in range(d)]
-        for dim in range(d):
-            level[dim] = combiScheme[lkeys[dim]]
-        # check if current simulation is finished
-        if sim_launcher.check_finished(level_vector=level):
-            # if yes, check if simulation time is reached
-            if Qes_data(level_vector=level).how_long_run() > 0.99 * newStartTime:
-                # if yes, remove from list
-                combiScheme.drop(index=i, inplace=True)
-            else:
-                # if no, prolong the simulation again
-                sim_launcher.restart_sim(
-                    level_vector=level, newSimTime=newEndTime)
-    # sleep for a minute, so we don't burn energy absolutely unnecessarily
-    time.sleep(60)
+    while len(combiScheme) > 0:
+        for i, row in combiScheme.iterrows():
+            for dim in range(d):
+                level[dim] = row[lkeys[dim]]
+            # check if current simulation is finished
+            if sim_launcher.check_finished(level_vector=level):
+                # if yes, check if simulation time is reached
+                if Qes_data(level_vector=level).how_long_run() > 0.99 * newEndTime:
+                    # if yes, remove from list
+                    combiScheme.drop(index=i, inplace=True)
+                else:
+                    # if no, prolong the simulation again
+                    sim_launcher.restart_sim(
+                        level_vector=level, newSimTime=newEndTime)
+        # sleep for a minute, so we don't burn energy absolutely unnecessarily
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    combiSchemeMode = sys.argv[1]
+    newEndTime = sys.argv[2]
